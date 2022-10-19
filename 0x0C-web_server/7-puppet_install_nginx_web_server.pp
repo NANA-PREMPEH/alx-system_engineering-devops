f4a08579098a810e2b064d6d1811e7379802b211
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-10-19 00:37:56 +0000

    Create 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+# Practice configuring a server with Puppet! Just as you did before, 
+# install and configure an Nginx server using Puppet instead of Bash. To save time and effort, include resources in 
+# the manifest to perform a 301 redirect when querying /redirect_me.
+# Manifest to configure an Ubuntu server with nginx
+
+# Install nginx
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+#Added a file index-.html
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!'
+}
+
+#Redirect to another file
+file_line { 'Redirection':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+}
+
+# Restart service
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Redirection']
+}
